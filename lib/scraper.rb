2aa16aebb9462c397b4bd89cac7fe1e3b4823a23
Jeff Bai <j.bai@wustl.edu>
2020-06-16 00:51:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,15 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open('fixtures/student-site/index.html')
+    doc = Nokogiri::HTML(html)
   end
+
+  students = []
 
   def self.scrape_profile_page(profile_url)
     
