bf30ae9fd0c2d5f18db9881dc933a96482720b0c
Jeff Bai <j.bai@wustl.edu>
2020-06-16 00:53:29 +0000

    Automatically backed up by Learn

---
+++ lib/scraper.rb
@@ -0,0 +1,26 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+class Scraper
+
+  def self.scrape_index_page(index_url)
+    html = open('fixtures/student-site/index.html')
+    doc = Nokogiri::HTML(html)
+
+    scraped_students = []
+    students = doc.css(".student-card")
+    students.each do |student|
+      name = student.css(".student-name").text
+      location = student.css(".student-location").text
+      profile_url = student.css("a").attr("href").text
+      hash = {:name => name, :location => location, :profile_url => profile_url}
+    }
+    scraped_students << hash
+  end
+
+  def self.scrape_profile_page(profile_url)
+
+  end
+
+end
